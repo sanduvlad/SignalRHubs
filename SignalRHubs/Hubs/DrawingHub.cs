469b4ca079a6a30afe675bf32888db428f984d5e
Sandu Vlad <sanduvlad79@gmail.com>
2018-09-12 10:18:46 +0300

    first commit

---
+++ SignalRHubs/Hubs/DrawingHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRHubs.Hubs
+{
+    public class DrawingHub : Hub
+    {
+        private static int connections = 0;
+        public async Task SendNewCoordinate(int x, int y)
+        {
+            await Clients.Others.SendAsync("ReceiveNewCoordinate", x, y);
+        }
+
+        public async Task SendMouseClickedEvent()
+        {
+            await Clients.Others.SendAsync("ReceiveMouseClickedEvent");
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            connections++;
+            Clients.All.SendAsync("UpdateNumberOfConnections", connections);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            connections--;
+            Clients.All.SendAsync("UpdateNumberOfConnections", connections);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task ResetCanvas()
+        {
+            await Clients.Others.SendAsync("ResetCanvasEventReceived");
+        }
+    }
+}
---
+++ SignalRHubs/wwwroot/js/draw/drawlogic.js
@@ -0,0 +1,117 @@
+﻿
+var isDrawing = false;
+var resetStartPoint = true;
+var canvas = document.getElementById("drawingArea");
+var ctx = canvas.getContext("2d");
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/DrawWithMe").build();
+
+
+connection.on("ReceiveNewCoordinate", function (x, y) {
+    ReceiveNewCoordinate(x,y);
+});
+
+connection.on("ReceiveMouseClickedEvent", function () {
+    MouseClickedEventReceived();
+});
+
+connection.on("UpdateNumberOfConnections", function (conns) {
+    $("#lblNumberOfSessions").text("Number of active connections: " + conns);
+});
+
+connection.on("ResetCanvasEventReceived", function () {
+    ResetCanvas();
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+function ResetButtonPressed() {
+    console.log("Reset button pressed");
+    ResetCanvas();
+    connection.invoke("ResetCanvas");
+}
+
+function ResetCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+
+function MouseDownEvent() {
+    //console.log("MouseDownEvent");
+}
+
+function MouseUpEvent() {
+    //console.log("MouseUpEvent");
+    console.log(event.clientX - $("#drawingArea").offset().left);
+    console.log(event.clientY - $("#drawingArea").offset().top);
+}
+
+function MouseMovedEvent() {
+    MouseMovedEvent(undefined, undefined);
+}
+
+function MouseMovedEvent(x,y) {
+    var drawX = 0;
+    var drawY = 0;
+    if (x !== undefined && y !== undefined) {
+        drawX = x;
+        drawY = y;
+    }
+    else {
+        drawX = event.clientX - $("#drawingArea").offset().left;
+        drawY = event.clientY - $("#drawingArea").offset().top;
+    }
+    if (isDrawing == true) {
+        if (resetStartPoint == true) {
+            resetStartPoint = false;
+            ctx.beginPath();
+            ctx.moveTo(drawX, drawY);
+        }
+
+        ctx.lineTo(drawX, drawY);
+        ctx.stroke();
+
+        connection.invoke("SendNewCoordinate", drawX, drawY);
+    }
+
+}
+
+function ReceiveNewCoordinate(x,y) {
+    var drawX = 0;
+    var drawY = 0;
+    if (x !== undefined && y !== undefined) {
+        drawX = x;
+        drawY = y;
+    }
+    else {
+        drawX = event.clientX - $("#drawingArea").offset().left;
+        drawY = event.clientY - $("#drawingArea").offset().top;
+    }
+    if (isDrawing == true) {
+        if (resetStartPoint == true) {
+            resetStartPoint = false;
+            ctx.beginPath();
+            ctx.moveTo(drawX, drawY);
+        }
+        
+        ctx.lineTo(drawX, drawY);
+        ctx.stroke();
+    }
+}
+
+function MouseClickedEvent() {
+    isDrawing = !isDrawing;
+    resetStartPoint = isDrawing;
+    console.log("Mouse Clicked!:" + isDrawing);
+
+    connection.invoke("SendMouseClickedEvent");
+}
+
+function MouseClickedEventReceived() {
+    isDrawing = !isDrawing;
+    resetStartPoint = isDrawing;
+    console.log("Mouse Clicked!:" + isDrawing);
+}
